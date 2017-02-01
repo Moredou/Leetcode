bf0215ff41268e58d33464956b17892bcacbfb08
Moredou <422210880@qq.com>
2017-02-01 08:18:31 -0500

    Add files via upload

---
+++ 1 two sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var hash = {};
+
+    for(var i = 0; i < nums.length; i++) {
+        var num = nums[i];
+        if(hash[num] !== undefined) {
+            return [hash[num], i]
+        } else {
+            hash[target - num] = i;
+        }
+    }
+
+    return [];
+};
